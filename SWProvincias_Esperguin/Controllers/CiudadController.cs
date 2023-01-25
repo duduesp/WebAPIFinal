1afb2d5e0ae84091ea141a3216a1c48b73182348
duduesp <dudu.es@hotmail.com.ar>
2023-01-25 16:41:57 -0300

    pruebas CRUD Ciudad

---
+++ SWProvincias_Esperguin/Controllers/CiudadController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SWProvincias_Esperguin.Data;
+using SWProvincias_Esperguin.Models;
+namespace SWProvincias_Esperguin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CiudadController : ControllerBase
+    {
+        private readonly DBPaisContext context;
+
+        public CiudadController(DBPaisContext context)
+        {
+            this.context = context;
+        }
+
+        //GET: api/ciudad
+        [HttpGet]
+        public ActionResult<IEnumerable<Ciudad>> Get()
+        {
+            return context.Ciudades.ToList();
+        }
+
+        //POST api/ciudad
+        [HttpPost]
+        public ActionResult Post(Ciudad ciudad)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Ciudades.Add(ciudad);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //PUT api/ciudad/2
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Ciudad ciudad)
+        {
+            if (id != ciudad.IdCiudad)
+            {
+                return BadRequest();
+            }
+            context.Entry(ciudad).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+        //DELETE api/ciudad/1
+        [HttpDelete("{id}")]
+        public ActionResult<Ciudad> Delete(int id)
+        {
+            var ciudad = (from a in context.Ciudades
+                          where a.IdCiudad == id
+                          select a).SingleOrDefault();
+            if (ciudad == null)
+            {
+                return NotFound();
+            }
+            context.Ciudades.Remove(ciudad);
+            context.SaveChanges();
+            return ciudad;
+        }
+    }
+}
